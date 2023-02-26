6ce16b9dc6c45230830956233cc08d047c4e2666
Double0negative <d.f@superleague.com>
2023-02-25 22:18:10 -0500

    0 if no game

--- frontend/src/utils/statsUtils.js
+++ frontend/src/utils/statsUtils.js
@@ -103,7 +103,7 @@ function getStatsByMode(teams, mode, overall) {
     } else {
         console.log("teams", JSON.stringify(teams));
         let result = _(teams)
-            .map(team => [...team.player_stats.map(p => ({ ...p, score: overall?.games ? _.sum(overall.games.map(g => g.teams.find(t => t.player_stats.find(pl => pl.playerId == p.playerId)).overall_stats.score)) : team.overall_stats.score }))])
+            .map(team => [...team.player_stats.map(p => ({ ...p, score: overall?.games ? _.sum(overall.games.map(g => g?.teams?.find(t => t.player_stats.find(pl => pl.playerId == p.playerId))?.overall_stats?.score ?? 0)) : team.overall_stats.score }))])
             .flatten()
             .groupBy("playerId")
             .map(player => player.reduce((val, cur) => {
